f445f0eb446eb291bac9fde0e7ebb63235ce17c8
salob <sianob79@gmail.com>
2025-04-09 00:30:04 +0100

    fix syntax

--- src/main/groovy/com/bp/api/controller/BloodPressureController.groovy
+++ src/main/groovy/com/bp/api/controller/BloodPressureController.groovy
@@ -4,8 +4,7 @@ import com.bp.api.model.BloodPressureRequest
 import org.springframework.web.bind.annotation.*
 import org.springframework.http.ResponseEntity
 
-@CrossOrigin(origins = "http://localhost:5173")
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = ["http://localhost:8080", "http://localhost:5173"])
 @RestController
 //@CrossOrigin
 class BloodPressureController {
