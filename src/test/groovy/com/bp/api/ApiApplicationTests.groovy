4ee08a2b0ee03484cdd9081bda84c888eba31393
salob <sianob79@gmail.com>
2025-04-18 19:30:16 +0100

    add some tests

--- src/test/groovy/com/bp/api/ApiApplicationTests.groovy
+++ src/test/groovy/com/bp/api/ApiApplicationTests.groovy
@@ -1,13 +1,23 @@
 package com.bp.api
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull
 
 @SpringBootTest
 class ApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Autowired
+    private ApiApplication apiApplication
+
+    @Test
+    void testApiApplicationBeanLoaded() {
+        assertNotNull(apiApplication, "ApiApplication bean should be loaded in the context")
+    }
 }
+
---
+++ src/test/groovy/com/bp/api/ExampleTest.groovy
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExampleTest {
+    @Test
+    void testHelloWorld() {
+        assertEquals(2, 1 + 1);
+    }
+}
